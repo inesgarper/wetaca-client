389b5ff8ac75aa8baf7d850118ff70c0b85ddb73
Guille Ávila <guille9797@hotmail.com>
2022-09-27 19:34:07 +0200

    material ui navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,13 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import reviewServices from './app/services/reviewServices';
-import { getReviewsQuery_getReviews } from './app/services/reviewServices/queries/__generated__/getReviewsQuery';
+import NavBar from './app/components/NavBar/NavBar';
 
 
 function App() {
 
-  const [mealDetails, setMealDetails] = useState<Array<getReviewsQuery_getReviews | null> | null>(null)
-
-  // const [meals, setMeals] = useState<Array<getAllMealsQuery_getAllMeals | null>>([])
-
-  useEffect(() => {
-    reviewServices
-      .getReviews('630d14cef19d5545b990ec85')
-      .then(data => {
-        console.log('la respuesta', data)
-        setMealDetails(data)
-      })
-      .catch(err => console.log(err))
-
-    // mealServices
-    //   .getAllMeals()
-    //   .then(data => {
-    //     if (data) {
-    //       setMeals(data)
-    //     }
-    //     console.log(data)
-    //   })
-    //   .catch(err => console.log(err))
-  }, [])
-
   return (
     <div className="App">
+      <NavBar />
       <h1>TENGO EL CONTROL</h1>
-
-      {
-        mealDetails?.map(review => {
-          return <h3>{review?.comment}</h3>
-        })
-      }
-      <p></p>
     </div>
   );
 }
