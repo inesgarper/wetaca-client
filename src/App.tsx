9cf1f8a5db9b935c9526812d7287f70c9725a42b
inesgarper <igperianez@gmail.com>
2022-10-03 19:06:33 +0200

    createMealForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import { useContext } from 'react';
 import './App.css';
 import CreateMealForm from './app/components/CreateMealForm';
 import NavBar from './app/components/NavBar/NavBar';
-import NewMealForm from './app/components/NewMealForm/NewMealForm';
+import { AuthContext } from './app/contexts/auth.context';
 import AppRoutes from './app/routes/AppRoutes';
 
 
 function App() {
 
+  console.log(useContext(AuthContext))
+
   return (
     <div className="App">
       <NavBar />
       <AppRoutes />
-      <NewMealForm></NewMealForm>
       <CreateMealForm />
     </div>
   );
